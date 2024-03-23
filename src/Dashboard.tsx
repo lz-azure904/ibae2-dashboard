a93102c88c6b504ea0d57dca4a318dc2655d0696
lz-azure904 <zleon904@gmail.com>
2024-03-23 00:22:23 -0500

    update redirect url to dashboard.cloudwave3.com

--- src/Dashboard.tsx
+++ src/Dashboard.tsx
@@ -14,7 +14,7 @@ const LogoutButton: React.FC = () => {
     const { instance } = useMsal();
 
     return (
-        <button className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-green-700 rounded-lg hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800" onClick={() => instance.logoutRedirect({ postLogoutRedirectUri: "http://localhost:3000" })}>
+        <button className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-green-700 rounded-lg hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800" onClick={() => instance.logoutRedirect({ postLogoutRedirectUri: "https://dashboard.cloudwave3.com" })}>
         Logout
         </button>
     );
